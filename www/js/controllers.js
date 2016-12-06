c7efbd4fc596813a5a4ed8d0113bf03f41a29b06
Cameron Johnston <Cameron Johnston>
2016-12-05 23:44:01 -0700

    updated with sidebar, improved nav

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,34 +1,26 @@
 angular.module('acimsApp.controllers', [])
 
 .controller('MainCtrl', function($http, $scope, $state, $ionicSideMenuDelegate) {
-  console.log('MainCtrl is working');
   $scope.openMenu = function() {
     $ionicSideMenuDelegate.toggleLeft();
   };
-  console.log($state);
 })
 .controller('SearchCtrl', function($http, listService, $scope, $state) {
-  console.log('SearchCtrl is working');
   $scope.pullList = function() {
     $scope.data = listService.getList();
   };
-
   $scope.pullList();
-
 })
 
 .controller('AboutCtrl', function($http, $scope, $state) {
-  console.log('AboutCtrl is working');
-})
+  })
 
 .controller('HomeCtrl', function($http, $scope, $state) {
-  console.log('HomeCtrl is working');
-})
+  })
 
 
 
 .controller('speciesCtrl', function($http, $scope, $stateParams, speciesService, $state) {
-  //  console.log('speciesCtrl is working');
   //get ID
   $scope.id = $stateParams.ID;
   //Pull JSON for species
