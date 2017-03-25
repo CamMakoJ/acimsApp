a00f00aba7cbaa0dc4c8c07ba4338a90f573086e
Cameron Johnston <Cameron Johnston>
2017-03-25 14:54:55 -0600

    updated to include search memory, added rankings

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,38 +1,51 @@
 angular.module('acimsApp.controllers', [])
 
-.controller('MainCtrl', function($http, $scope, $state, $ionicSideMenuDelegate) {
-  $scope.openMenu = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
-})
-.controller('SearchCtrl', function($http, listService, $scope, $state) {
-  $scope.filterInput = '';
-
-  $scope.pullList = function() {
-    $scope.data = listService.getList();
-  };
-  $scope.pullList();
-})
-
-.controller('AboutCtrl', function($http, $scope, $state) {
+  .controller('MainCtrl', function($http, $scope, $state, $ionicSideMenuDelegate) {
+    $scope.openMenu = function() {
+      $ionicSideMenuDelegate.toggleLeft();
+    };
   })
 
-.controller('HomeCtrl', function($http, $scope, $state) {
-  function onDeviceReady() {
-    admob.createBannerView({publisherId: "ca-app-pub-5841639947461001/2632111177"});
-  }
-
-  document.addEventListener('deviceready', onDeviceReady, false);
+  .controller('SearchCtrl', function($http, listService, $scope, $state, $rootScope) {
+    $scope.updateFilter = function() {
+      $rootScope.filterInput = $scope.filterInput;
+    };
 
 
-})
+    $scope.pullList = function() {
+      $scope.data = listService.getList();
+    };
+    $scope.pullList();
+  })
 
-.controller('speciesCtrl', function($http, $scope, $stateParams, speciesService, $state) {
-  //get ID
-  $scope.id = $stateParams.ID;
-  //Pull JSON for species
-  $scope.pullSpecies = function(id) {
-    $scope.data = speciesService.getSpecies(id);
-  };
-  $scope.pullSpecies($scope.id);
-});
+  .controller('AboutCtrl', function($http, $scope, $state) {
+    $scope.sendMail = function() {
+      var mailWindow = window.open('mailto:Cam@MakoJ.com?subject=ACIMS App');
+      mailWindow.close();
+    };
+  })
+
+  .controller('HomeCtrl', function($http, $scope, $state) {
+    function onDeviceReady() {
+      admob.createBannerView({
+        publisherId: "ca-app-pub-5841639947461001/2632111177"
+      });
+    }
+
+    document.addEventListener('deviceready', onDeviceReady, false);
+
+
+  })
+
+  .controller('speciesCtrl', function($http, $scope, $stateParams, speciesService, $state) {
+    //get ID
+    $scope.id = $stateParams.ID;
+    //Pull JSON for species
+    $scope.pullSpecies = function(id) {
+      $scope.data = speciesService.getSpecies(id);
+    };
+    $scope.pullSpecies($scope.id);
+  })
+
+  .controller('rankingsCtrl', function($scope) {
+  });
